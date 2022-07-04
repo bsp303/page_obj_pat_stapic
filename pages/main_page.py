a224299ea5be847f9f51f6ffe5e291425422a010
LuckPC <bsp303@Gmail.com>
2022-07-04 14:26:56 +0600

    Переработка test_main_page в PageObj и исправление ошибки в main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 # Импортируем базовый класс BasePage
 from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
 # MainPage наследник BasePage (MainPage имеет полный доступ к всем атрибутам и методам класса-предка)
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
