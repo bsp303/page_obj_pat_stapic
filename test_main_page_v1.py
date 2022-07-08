c25dd842099ac1de4afc214b60ad0ad52185015a
LuckPC <bsp303@Gmail.com>
2022-07-08 17:13:06 +0600

    429 переход происходит неявно, страницу инициализируем в теле теста

--- test_main_page_v1.py
+++ test_main_page_v1.py
@@ -1,7 +1,7 @@
 # испортируем класс MainPage из файла main_page.py
 from .pages.main_page_v1 import MainPageV1
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page_v1(browser):
     link = "http://selenium1py.pythonanywhere.com/"    # Задаем первоначальную ссылку
     page = MainPageV1(browser, link)    # Инициализация Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()     # Открываем страницу при помощи метода open что в base_page
