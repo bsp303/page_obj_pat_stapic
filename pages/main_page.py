909ab4956e453f120405f818fb327ca001e2ff99
LuckPC <bsp303@Gmail.com>
2022-07-08 15:47:03 +0600

    428 в приведение файлов в порядок

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,18 +1,16 @@
 # Импортируем базовый класс BasePage
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 # Импорт класса с локаторами
 from .locators import MainPageLocators
-# Импорт класса LoginPage
+
+# Возврат нужного Page Object - Импорт класса LoginPage
 from .login_page import LoginPage
 
 # MainPage наследник BasePage (MainPage имеет полный доступ к всем атрибутам и методам класса-предка)
 class MainPage(BasePage):
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-        # Возвращаем url страницы на которую перешли в результате клика
-        #return LoginPage(browser=self.browser, url=self.browser.current_url)
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)    # Сохраняем селектор в переменную
+        link.click()    # Кликаем селектор
 
     def should_be_login_link(self):
         #символ *, он указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,10 @@
 import time
-
-from selenium.webdriver.common.by import By
-# импорт каласа описывающего главную страницу
-from .pages.main_page import MainPage
-# испортируем LoginPage
+# испортируем класс LoginPage из файла login_page.py
 from .pages.login_page import LoginPage
 
 def test_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = LoginPage (browser, link) # инициализация Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()    #открываем страницу
+    link = "http://selenium1py.pythonanywhere.com/" # Задаем основную ссылку (ссылка откроется после запуска браузера)
+    page = LoginPage (browser, link) # Инициализация Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()    # Открываем страницу
     time.sleep(2)
     page.should_be_login_page() # выполняем метод страницы - переходим на страницу логина
